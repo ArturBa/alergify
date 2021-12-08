78e4c51a2c786211936a26fa2aecea2f34989f4d
Artur Bauer <bauerart1@gmail.com>
2021-12-08 16:24:05 +0100

    feat(products): :sparkles: add search by barcode and name (#14)

--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -1,37 +1,73 @@
-import { getRepository, In } from 'typeorm';
+import { FindManyOptions, getRepository, In, Like } from 'typeorm';
 import { CreateProductDto } from '@dtos/products.dto';
 import { IngredientEntity } from '@entity/ingredients.entity';
 import { ProductEntity } from '@entity/products.entity';
-import { Product } from '@interfaces/products.interface';
+import { Product, ProductGetRequest } from '@interfaces/products.interface';
+import { PaginateResponse } from '@interfaces/internal/response.interface';
 import { checkIfConflict, checkIfEmpty } from './common.service';
+import { GetParamsBuilder } from './internal/get-params-builder';
+
+class GetProductQueryBuilder extends GetParamsBuilder<
+  ProductEntity,
+  ProductGetRequest
+> {
+  constructor() {
+    super();
+    this.query = {
+      ...this.query,
+      select: ['id', 'name', 'barcode'],
+    };
+  }
+
+  build(request: ProductGetRequest): void {
+    this.addPaginate(request);
+    this.addBarcode(request);
+    this.addName(request);
+  }
+
+  getTotal(): FindManyOptions<ProductEntity> {
+    const totalQuery = { ...this.query };
+    delete totalQuery.skip;
+    delete totalQuery.take;
+    return totalQuery;
+  }
+
+  protected addPaginate({ start, limit }: ProductGetRequest) {
+    this.query = {
+      ...this.query,
+      skip: start,
+      take: limit,
+    };
+  }
+
+  protected addName({ name }: ProductGetRequest): void {
+    if (name) {
+      this.appendWhere({ name: Like(`%${name}%`) });
+    }
+  }
+
+  protected addBarcode({ barcode }: ProductGetRequest): void {
+    if (barcode) {
+      this.appendWhere({ barcode });
+    }
+  }
+}
 
 class ProductsService {
   public products = ProductEntity;
 
   public ingredients = IngredientEntity;
 
-  public async findProductByQuery(query: string): Promise<Product[]> {
-    checkIfEmpty(query);
-
+  public async findProduct(
+    request: ProductGetRequest,
+  ): Promise<PaginateResponse<Partial<Product>>> {
     const productRepository = getRepository(this.products);
-    const products = await productRepository.find({
-      where: { name: query },
-      select: ['id', 'name'],
-    });
+    const queryBuilder = new GetProductQueryBuilder();
+    queryBuilder.build(request);
+    const data = await productRepository.find(queryBuilder.get());
+    const total = await productRepository.count(queryBuilder.getTotal());
 
-    return products;
-  }
-
-  public async getProductByBarcode(barcode: number): Promise<Product> {
-    checkIfEmpty(barcode);
-
-    const productRepository = getRepository(this.products);
-    const products = await productRepository.findOne({
-      where: { barcode },
-      select: ['id', 'name'],
-    });
-
-    return products;
+    return { data, total };
   }
 
   public async getProductById(productId: number): Promise<Partial<Product>> {
